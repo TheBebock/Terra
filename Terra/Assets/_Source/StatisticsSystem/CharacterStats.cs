a062071acdfcafa1ef52272fc9398471bafa805d
Mateusz Lewandowski <matlewsp4@wp.pl>
2024-11-24 11:45:13 +0100

    Adds prototyp CharacterStats

---
+++ Terra/Assets/_Source/StatisticsSystem/CharacterStats.cs
@@ -0,0 +1,21 @@
+using Core.ModifiableValue;
+using UnityEngine;
+
+namespace _Source.StatisticsSystem 
+{
+    [DisallowMultipleComponent]
+    public class CharacterStats : MonoBehaviour
+    {
+        [Header("Character Stats")]
+        [SerializeField] private ModifiableValue strength;
+        [SerializeField] private ModifiableValue maxHealth;
+        [SerializeField] private ModifiableValue speed;
+
+        public CharacterStats(float basestrength, float basemaxHealth, float basespeed)
+        {
+            strength = new ModifiableValue(basestrength);
+            maxHealth = new ModifiableValue(basemaxHealth);
+            speed = new ModifiableValue(basespeed);
+        }
+    }
+}
