ab4c8e2d66c432cc9f4c0c5041ee44e922138c31
Paw0lo151 <>
2024-12-08 17:45:16 +0100

    Adds IdleState methods

--- Terra/Assets/_Source/AI/Data/States/IdleState.cs
+++ Terra/Assets/_Source/AI/Data/States/IdleState.cs
@@ -8,26 +8,69 @@ public class IdleState : AIState
 {
     public override void StartState(AIController controller)
     {
-        throw new System.NotImplementedException();
+        Debug.Log("Entering idle state");
+
+        controller.GetComponent<Animator>()?.Play("Idle");
     }
 
     public override void UpdateState(AIController controller)
     {
+        float detectionRange = 10f;
+        Collider[] hits = Physics.OverlapSphere(controller.transform.position, detectionRange);
+
+        for (int i = 0; i < hits.Length; i++)
+        {
+            Collider hit = hits[i];
+            if (hit.CompareTag("Player"))
+            {
+                Debug.Log("Player detected, transition");
+                break;
+            }
+        }
+
         throw new System.NotImplementedException();
     }
 
     public override void ExitState(AIController controller)
     {
-        throw new System.NotImplementedException();
+        Debug.Log("Exiting idle state");
+        Animator animator = controller.GetComponent<Animator>();
+        if (animator != null && animator.GetCurrentAnimatorStateInfo(0).IsName("Idle"))
+        {
+            animator.Play("Empty");
+        }
     }
 
     public override bool CanExitState(AIController controller)
     {
-        throw new System.NotImplementedException();
+        float detectionRange = 10f;
+        Collider[] hits = Physics.OverlapSphere(controller.transform.position, detectionRange);
+
+        for (int i = 0; i < hits.Length; i++)
+        {
+            Collider hit = hits[i];
+            if (hit.CompareTag("Player"))
+            {
+                Debug.Log("Player detected, can exit IdleState.");
+                return true;
+            }
+        }
+
+        Debug.Log("No player detected, cannot exit IdleState.");
+        return false;
     }
 
     public override bool CanEnterState(AIController controller)
     {
-        throw new System.NotImplementedException();
+
+
+        //    if (nie ma spalnionych warunkow do wejscia)
+        {
+            //      return false;
+        }
+
+        //sa warunki do wejscia
+        return true;
+
     }
 }
